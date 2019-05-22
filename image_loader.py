a59c5fccf43f9551a4f86881d8b20d566cef75e9
benjamin362 <50482282+benjamin362@users.noreply.github.com>
2019-05-22 13:32:26 +0200

    first_commit

--- image_loader.py
+++ image_loader.py
@@ -1,23 +1,24 @@
 
 # coding: utf-8
 
-# In[13]:
+# In[5]:
 
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import load_img
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
 
 
-# In[25]:
+# In[6]:
 
 
 # we create two instances with the same arguments
-data_gen_args = dict(featurewise_center=True,
-                     featurewise_std_normalization=True,
-                     rotation_range=90,
-                     width_shift_range=0.1,
-                     height_shift_range=0.1,
-                     zoom_range=0.2)
+data_gen_args = dict(featurewise_center=False,
+                     featurewise_std_normalization=False,
+                     )
 
 image_datagen = ImageDataGenerator(**data_gen_args)
 mask_datagen = ImageDataGenerator(**data_gen_args)
@@ -26,13 +27,33 @@ mask_datagen = ImageDataGenerator(**data_gen_args)
 seed = 1
 
 image_generator = image_datagen.flow_from_directory(
-    'ISBI2016_ISIC_Part1_Training_Data',
-    class_mode=None,)
+    'train_frames/all',
+    batch_size=32,
+    class_mode=None,
+    target_size = (767,1022),
+    color_mode = 'grayscale'
+    )
 
 mask_generator = mask_datagen.flow_from_directory(
-    'ISBI2016_ISIC_Part1_Training_GroundTruth',
-    class_mode=None,)
+    'train_masks/',
+    batch_size=32,
+    class_mode=None,
+    target_size = (1022,767),
+    color_mode = 'grayscale'
+    )
 
 # combine generators into one which yields image and masks
 train_generator = zip(image_generator, mask_generator)
+
+
+# In[ ]:
+
+
+im_0 = image_generator[0][0]
+
+
+# In[4]:
+
+
+plt.imshow(im_0.reshape(767,1022))
 
